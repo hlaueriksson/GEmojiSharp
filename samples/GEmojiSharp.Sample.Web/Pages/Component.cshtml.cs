6cd169b965082a482c1930a6374a84e19f8bf1fb
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2019-04-18 21:40:30 +0200

    :information_desk_person:

---
+++ samples/GEmojiSharp.Sample.Web/Pages/Component.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GEmojiSharp.Sample.Web.Pages
+{
+    public class ComponentModel : PageModel
+    {
+        public string Emoji { get; set; }
+        public string Content { get; set; }
+
+        //private readonly ITagHelperComponentManager _tagHelperComponentManager;
+
+        //public ComponentModel(ITagHelperComponentManager tagHelperComponentManager)
+        //{
+        //    _tagHelperComponentManager = tagHelperComponentManager;
+        //}
+
+        public void OnGet()
+        {
+            //_tagHelperComponentManager.Components.Add(new BodyTagHelperComponent());
+
+            Emoji = ":open_umbrella:";
+            Content = "it's raining :cat:s and :dog:s!";
+        }
+    }
+}
