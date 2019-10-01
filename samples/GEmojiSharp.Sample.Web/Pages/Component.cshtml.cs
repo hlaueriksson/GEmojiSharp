b12c4f3237fd182d54f7c81aef283788078b3764
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2019-10-01 23:26:39 +0200

    :recycle:

--- samples/GEmojiSharp.Sample.Web/Pages/Component.cshtml.cs
+++ samples/GEmojiSharp.Sample.Web/Pages/Component.cshtml.cs
@@ -5,7 +5,7 @@ namespace GEmojiSharp.Sample.Web.Pages
     public class ComponentModel : PageModel
     {
         public string Emoji { get; set; }
-        public string Content { get; set; }
+        public string Text { get; set; }
 
         //private readonly ITagHelperComponentManager _tagHelperComponentManager;
 
@@ -19,7 +19,7 @@ namespace GEmojiSharp.Sample.Web.Pages
             //_tagHelperComponentManager.Components.Add(new BodyTagHelperComponent());
 
             Emoji = ":open_umbrella:";
-            Content = "it's raining :cat:s and :dog:s!";
+            Text = "it's raining :cat:s and :dog:s!";
         }
     }
 }
--- samples/GEmojiSharp.Sample.Web/Pages/Index.cshtml.cs
+++ samples/GEmojiSharp.Sample.Web/Pages/Index.cshtml.cs
@@ -5,12 +5,12 @@ namespace GEmojiSharp.Sample.Web.Pages
     public class IndexModel : PageModel
     {
         public string Emoji { get; set; }
-        public string Content { get; set; }
+        public string Text { get; set; }
 
         public void OnGet()
         {
             Emoji = ":open_umbrella:";
-            Content = "it's raining :cat:s and :dog:s!";
+            Text = "it's raining :cat:s and :dog:s!";
         }
     }
 }
