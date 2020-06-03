c828d93cf56a5acc732bb899ebd5bfbc1e62f2dd
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2020-06-03 19:57:11 +0200

    :wastebasket:

--- src/GEmojiSharp.TagHelpers/EmojiExtensions.cs
+++ src/GEmojiSharp.TagHelpers/EmojiExtensions.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Text.RegularExpressions;
-
-[assembly: InternalsVisibleTo("GEmojiSharp.Tests")]
 
 namespace GEmojiSharp.TagHelpers
 {
