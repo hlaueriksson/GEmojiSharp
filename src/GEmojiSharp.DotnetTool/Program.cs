265d28ee671e3702e884a621369793089449fcad
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2022-04-14 20:34:09 +0200

    :rotating_light:

--- src/GEmojiSharp.DotnetTool/Program.cs
+++ src/GEmojiSharp.DotnetTool/Program.cs
@@ -6,6 +6,7 @@ using TextCopy;
 Console.OutputEncoding = Encoding.UTF8;
 
 var argument = new Argument<string[]>("args", "Find emojis via description, category, alias or tag");
+
 var copyOption = new Option<bool>(new[] { "-c", "--copy" }, "Copy to clipboard");
 
 // raw
@@ -14,17 +15,22 @@ var rawCommand = new Command("raw", "Get raw emojis")
     argument,
     copyOption,
 };
-rawCommand.AddAlias("r");
-rawCommand.SetHandler((string[] args, bool copy) =>
-{
-    var value = string.Join(" ", args);
-    var emojis = Emoji.Find(value);
 
-    foreach (var e in emojis)
-        Console.WriteLine(e.Raw);
-    if (copy)
-        ClipboardService.SetText(string.Join(string.Empty, emojis.Select(x => x.Raw)));
-}, argument, copyOption);
+rawCommand.AddAlias("r");
+
+rawCommand.SetHandler(
+    (string[] args, bool copy) =>
+    {
+        var value = string.Join(" ", args);
+        var emojis = Emoji.Find(value);
+
+        foreach (var e in emojis)
+            Console.WriteLine(e.Raw);
+        if (copy)
+            ClipboardService.SetText(string.Join(string.Empty, emojis.Select(x => x.Raw)));
+    },
+    argument,
+    copyOption);
 
 // alias
 var aliasCommand = new Command("alias", "Get emoji aliases")
@@ -32,94 +38,122 @@ var aliasCommand = new Command("alias", "Get emoji aliases")
     argument,
     copyOption,
 };
-aliasCommand.AddAlias("a");
-aliasCommand.SetHandler((string[] args, bool copy) =>
-{
-    var value = string.Join(" ", args);
-    var emojis = Emoji.Find(value);
 
-    foreach (var emoji in emojis)
-        foreach (var a in emoji.Aliases)
-            Console.WriteLine(a.PadAlias());
-    if (copy)
-        ClipboardService.SetText(string.Join(string.Empty, emojis.SelectMany(x => x.Aliases).Select(x => x.PadAlias())));
-}, argument, copyOption);
+aliasCommand.AddAlias("a");
+
+aliasCommand.SetHandler(
+    (string[] args, bool copy) =>
+    {
+        var value = string.Join(" ", args);
+        var emojis = Emoji.Find(value);
+
+        foreach (var emoji in emojis)
+        {
+            foreach (var a in emoji.Aliases)
+            {
+                Console.WriteLine(a.PadAlias());
+            }
+        }
+
+        if (copy)
+            ClipboardService.SetText(string.Join(string.Empty, emojis.SelectMany(x => x.Aliases).Select(x => x.PadAlias())));
+    },
+    argument,
+    copyOption);
 
 // emojify
 var emojifyArgument = new Argument<string[]>("args", "A text with emoji aliases");
+
 var emojifyCommand = new Command("emojify", "Replaces aliases with raw emojis")
 {
     emojifyArgument,
     copyOption,
 };
-emojifyCommand.AddAlias("e");
-emojifyCommand.SetHandler((string[] args, bool copy) =>
-{
-    var value = string.Join(" ", args);
-    var result = Emoji.Emojify(value);
 
-    Console.WriteLine(result);
-    if (copy)
-        ClipboardService.SetText(result);
-}, emojifyArgument, copyOption);
+emojifyCommand.AddAlias("e");
+
+emojifyCommand.SetHandler(
+    (string[] args, bool copy) =>
+    {
+        var value = string.Join(" ", args);
+        var result = Emoji.Emojify(value);
+
+        Console.WriteLine(result);
+        if (copy)
+            ClipboardService.SetText(result);
+    },
+    emojifyArgument,
+    copyOption);
 
 // demojify
 var demojifyArgument = new Argument<string[]>("args", "A text with raw emojis");
+
 var demojifyCommand = new Command("demojify", "Replaces raw emojis with aliases")
 {
     demojifyArgument,
     copyOption,
 };
-demojifyCommand.AddAlias("d");
-demojifyCommand.SetHandler((string[] args, bool copy) =>
-{
-    var value = string.Join(" ", args);
-    var result = Emoji.Demojify(value);
 
-    Console.WriteLine(result);
-    if (copy)
-        ClipboardService.SetText(result);
-}, demojifyArgument, copyOption);
+demojifyCommand.AddAlias("d");
+
+demojifyCommand.SetHandler(
+    (string[] args, bool copy) =>
+    {
+        var value = string.Join(" ", args);
+        var result = Emoji.Demojify(value);
+
+        Console.WriteLine(result);
+        if (copy)
+            ClipboardService.SetText(result);
+    },
+    demojifyArgument,
+    copyOption);
 
 // export
 var formatOption = new Option<string>(new[] { "-f", "--format" }, "Format the data as <json|toml|xml|yaml>");
+
 var exportCommand = new Command("export", "Export emoji data")
 {
     argument,
     formatOption,
     copyOption,
 };
-exportCommand.SetHandler((string[] args, string format, bool copy) =>
-{
-    var value = string.Join(" ", args);
-    var emojis = Emoji.Find(value);
-    string result;
 
-    if (!string.IsNullOrEmpty(format) && format.ToLower() == "yaml")
+exportCommand.SetHandler(
+    (string[] args, string format, bool copy) =>
     {
-        var serializer = new YamlDotNet.Serialization.SerializerBuilder().Build();
-        result = serializer.Serialize(emojis);
-    }
-    else if (!string.IsNullOrEmpty(format) && format.ToLower() == "xml")
-    {
-        var serializer = new System.Xml.Serialization.XmlSerializer(emojis.GetType());
-        var writer = new StringWriter();
-        serializer.Serialize(System.Xml.XmlWriter.Create(writer, new System.Xml.XmlWriterSettings { Indent = true }), emojis);
-        result = writer.ToString();
-    }
-    else if (!string.IsNullOrEmpty(format) && format.ToLower() == "toml")
-    {
-        result = Tomlyn.Toml.FromModel(emojis.ToDictionary(x => x.Alias()));
-    }
-    else
-    {
-        result = Newtonsoft.Json.JsonConvert.SerializeObject(emojis, Newtonsoft.Json.Formatting.Indented);
-    }
+        var value = string.Join(" ", args);
+        var emojis = Emoji.Find(value);
+        string result;
 
-    Console.WriteLine(result);
-    if (copy)
-        ClipboardService.SetText(result);
-}, argument, formatOption, copyOption);
+        if (!string.IsNullOrEmpty(format) && format.ToUpperInvariant() == "YAML")
+        {
+            var serializer = new YamlDotNet.Serialization.SerializerBuilder().Build();
+            result = serializer.Serialize(emojis);
+        }
+        else if (!string.IsNullOrEmpty(format) && format.ToUpperInvariant() == "XML")
+        {
+            var serializer = new System.Xml.Serialization.XmlSerializer(emojis.GetType());
+            var writer = new StringWriter();
+            serializer.Serialize(System.Xml.XmlWriter.Create(writer, new System.Xml.XmlWriterSettings { Indent = true }), emojis);
+            result = writer.ToString();
+        }
+        else if (!string.IsNullOrEmpty(format) && format.ToUpperInvariant() == "TOML")
+        {
+            result = Tomlyn.Toml.FromModel(emojis.ToDictionary(x => x.Alias()));
+        }
+        else
+        {
+            result = Newtonsoft.Json.JsonConvert.SerializeObject(emojis, Newtonsoft.Json.Formatting.Indented);
+        }
+
+        Console.WriteLine(result);
+        if (copy)
+            ClipboardService.SetText(result);
+    },
+    argument,
+    formatOption,
+    copyOption);
 
 // root
 var rootCommand = new RootCommand()
@@ -130,6 +164,7 @@ var rootCommand = new RootCommand()
     demojifyCommand,
     exportCommand,
 };
+
 rootCommand.Description = "GEmoji dotnet tool";
 
 return rootCommand.Invoke(args);
