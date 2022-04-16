c02b28710c9db5e8837ebd062d956a5ed460f05d
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2022-04-16 20:31:33 +0200

    :hammer:

--- test.bat
+++ test.bat
@@ -1,2 +1,3 @@
 dotnet test .\tests\GEmojiSharp.Tests\GEmojiSharp.Tests.csproj
 dotnet test .\tests\GEmojiSharp.Tests\GEmojiSharp.Tests.csproj --filter "Category=Integration"
+dotnet test .\tests\GEmojiSharp.PowerToysRun.Tests\GEmojiSharp.PowerToysRun.Tests.csproj
