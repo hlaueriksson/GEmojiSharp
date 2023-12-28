364470672c8baad739fe3ecb31b646c19056de2c
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2023-12-28 21:07:51 +0100

    :arrow_up:

--- samples/GEmojiSharp.Sample.BlazorServer/Program.cs
+++ samples/GEmojiSharp.Sample.BlazorServer/Program.cs
@@ -1,15 +1,17 @@
+using GEmojiSharp.Sample.BlazorServer.Components;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -17,10 +19,9 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
+app.UseAntiforgery();
 
-app.UseRouting();
-
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
 
 app.Run();
