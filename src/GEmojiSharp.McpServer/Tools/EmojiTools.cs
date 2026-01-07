909e5ba3bffb4dce38ae7ff041d486805f44b3a9
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2026-01-07 18:34:55 +0100

    UseStructuredContent

--- src/GEmojiSharp.McpServer/Tools/EmojiTools.cs
+++ src/GEmojiSharp.McpServer/Tools/EmojiTools.cs
@@ -4,15 +4,20 @@ using ModelContextProtocol.Server;
 
 internal class EmojiTools
 {
-    [McpServerTool]
+    [McpServerTool(UseStructuredContent = true)]
     [Description("Gets all emojis.")]
     public GEmojiResult[] GetAllEmojis() =>
         [.. Emoji.All.Select(x => x.ToResult())];
 
-    [McpServerTool]
+    [McpServerTool(UseStructuredContent = true)]
     [Description("Gets the emoji associated with the alias or raw Unicode string.")]
     public GEmojiResult GetEmoji([Description("The emoji alias or raw Unicode string.")] string value) =>
         Emoji.Get(value).ToResult();
+
+    [McpServerTool(UseStructuredContent = true)]
+    [Description("Returns emojis that match the Description, Category, Aliases or Tags.")]
+    public GEmojiResult[] Find([Description("The value to search for.")] string value) =>
+    [.. Emoji.Find(value).Select(x => x.ToResult())];
 
     [McpServerTool]
     [Description("Replaces emoji aliases with raw Unicode strings.")]
@@ -23,11 +28,6 @@ internal class EmojiTools
     [Description("Replaces raw Unicode strings with emoji aliases.")]
     public string Demojify([Description("A text with raw Unicode strings.")] string text) =>
         Emoji.Demojify(text);
-
-    [McpServerTool]
-    [Description("Returns emojis that match the Description, Category, Aliases or Tags.")]
-    public GEmojiResult[] Find([Description("The value to search for.")] string value) =>
-        [.. Emoji.Find(value).Select(x => x.ToResult())];
 }
 
 internal record GEmojiResult(string Raw, string? Description, string? Category, string[] Aliases, string[]? Tags, string[]? SkinTones, bool IsCustom);
