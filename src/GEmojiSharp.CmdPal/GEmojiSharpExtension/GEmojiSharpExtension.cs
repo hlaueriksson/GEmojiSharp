d36fbbc0faa6f53196eae9e2f2641eea69e34283
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2026-01-08 23:43:49 +0100

    clean

--- src/GEmojiSharp.CmdPal/GEmojiSharpExtension/GEmojiSharpExtension.cs
+++ src/GEmojiSharp.CmdPal/GEmojiSharpExtension/GEmojiSharpExtension.cs
@@ -1,7 +1,3 @@
-// Copyright (c) Microsoft Corporation
-// The Microsoft Corporation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
-
 using System;
 using System.Runtime.InteropServices;
 using System.Threading;
@@ -10,16 +6,9 @@ using Microsoft.CommandPalette.Extensions;
 namespace GEmojiSharpExtension;
 
 [Guid("0caf71f0-3ded-4475-b598-ad9304f89889")]
-public sealed partial class GEmojiSharpExtension : IExtension, IDisposable
+public sealed partial class GEmojiSharpExtension(ManualResetEvent ExtensionDisposedEvent) : IExtension, IDisposable
 {
-    private readonly ManualResetEvent _extensionDisposedEvent;
-
     private readonly GEmojiSharpExtensionCommandsProvider _provider = new();
-
-    public GEmojiSharpExtension(ManualResetEvent extensionDisposedEvent)
-    {
-        this._extensionDisposedEvent = extensionDisposedEvent;
-    }
 
     public object? GetProvider(ProviderType providerType)
     {
@@ -30,5 +19,5 @@ public sealed partial class GEmojiSharpExtension : IExtension, IDisposable
         };
     }
 
-    public void Dispose() => this._extensionDisposedEvent.Set();
+    public void Dispose() => ExtensionDisposedEvent.Set();
 }
