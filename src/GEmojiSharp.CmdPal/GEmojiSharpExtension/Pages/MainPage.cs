779d14e347e52da37170a4a7decd49250bc9961e
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2026-01-10 21:59:31 +0100

    wip

--- src/GEmojiSharp.CmdPal/GEmojiSharpExtension/Pages/MainPage.cs
+++ src/GEmojiSharp.CmdPal/GEmojiSharpExtension/Pages/MainPage.cs
@@ -91,8 +91,42 @@ internal sealed partial class MainPage : DynamicListPage
 
     private ListItem ToListItem(GEmoji emoji) => new()
     {
-        Title = emoji.Raw,
+        Icon = emoji.IsCustom ? IconHelpers.FromRelativePath($"Assets/Custom/{emoji.Filename}.png") : new IconInfo(emoji.Raw),
+        Title = string.Join(" ", emoji.Aliases.Select(x => x.PadAlias())),
         Subtitle = emoji.Description,
-        Command = new CopyTextCommand(emoji.Raw),
+        Command = emoji.IsCustom ? new NoOpCommand() : new CopyTextCommand(emoji.Raw),
+        MoreCommands = [.. GetMoreCommands(emoji)],
+        Tags = emoji.Tags != null ? [.. emoji.Tags.Select(x => new Tag(x))] : [],
+        Details = new Details
+        {
+            Title = "GEmoji",
+            Body = GetDetailsBody(emoji),
+        },
     };
+    private IEnumerable<IContextItem> GetMoreCommands(GEmoji emoji)
+    {
+        var title = emoji.Aliases.Length > 1 ? "Copy Aliases" : "Copy Alias";
+        yield return new CommandContextItem(new CopyTextCommand(string.Concat(emoji.Aliases.Select(x => x.PadAlias()))) { Name = title })
+        {
+            Title = title,
+        };
+
+        if (emoji.HasSkinTones)
+        {
+            yield return new CommandContextItem(new CopyTextCommand(emoji.Raw + string.Concat(emoji.RawSkinToneVariants())) { Name = "Copy SkinTones" })
+            {
+                Title = "Copy SkinTones",
+            };
+        }
+    }
+
+    private string GetDetailsBody(GEmoji emoji)
+    {
+        return
+            (emoji.Category != null ? $"**Category:** {emoji.Category}\n\n" : "") +
+            (emoji.UnicodeVersion != null ? $"**UnicodeVersion:** {emoji.UnicodeVersion}\n\n" : "") +
+            (emoji.IosVersion != null ? $"**IosVersion:** {emoji.IosVersion}\n\n" : "") +
+            (emoji.HasSkinTones ? $"**SkinTones:** {string.Concat(emoji.RawSkinToneVariants())}\n\n" : "") +
+            (emoji.IsCustom ? $"**IsCustom:** ✅" : "");
+    }
 }
