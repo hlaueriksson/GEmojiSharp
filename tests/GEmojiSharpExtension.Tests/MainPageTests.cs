d5cd0760914fd9489495dbb44e509531466f813e
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2026-01-12 10:49:52 +0100

    tests

--- tests/GEmojiSharpExtension.Tests/MainPageTests.cs
+++ tests/GEmojiSharpExtension.Tests/MainPageTests.cs
@@ -14,17 +14,43 @@ public class MainPageTests
     }
 
     [TestMethod]
-    public void GetItems()
+    public void GetItems_Emoji()
     {
         _subject.GetItems().Should().NotBeEmpty();
 
-        _subject.UpdateSearchText("", "globe showing");
-        _subject.GetItems().Should()
-            .Contain(x => x.Title == "🌍")
-            .And.Contain(x => x.Title == "🌎")
-            .And.Contain(x => x.Title == "🌏");
+        _subject.SearchText = "globe showing";
+        _subject.GetItems().Should().HaveCount(3)
+            .And.Contain(x => x.Title == ":earth_africa:")
+            .And.Contain(x => x.Title == ":earth_americas:")
+            .And.Contain(x => x.Title == ":earth_asia:");
 
-        _subject.UpdateSearchText("", "tada");
-        _subject.GetItems().Should().ContainSingle(x => x.Title == "🎉");
+        _subject.SearchText = "tada";
+        _subject.GetItems().Should().ContainSingle(x => x.Title == ":tada:");
+    }
+
+    [TestMethod]
+    public void GetItems_Category()
+    {
+        _subject.Filters.CurrentFilterId = SearchType.Category.ToString();
+        _subject.GetItems().Should().NotBeEmpty();
+
+        _subject.SearchText = "body";
+        _subject.GetItems().Should().HaveCount(1)
+            .And.Contain(x => x.Title == "People & Body");
+    }
+
+    [TestMethod]
+    public void GetItems_Transform()
+    {
+        _subject.Filters.CurrentFilterId = SearchType.Transform.ToString();
+        _subject.GetItems().Should().BeEmpty();
+
+        _subject.SearchText = "Hello, :earth_africa:";
+        _subject.GetItems().Should().HaveCount(1)
+            .And.Contain(x => x.Title == "Hello, 🌍");
+
+        _subject.SearchText = "Hello, 🌍";
+        _subject.GetItems().Should().HaveCount(1)
+            .And.Contain(x => x.Title == "Hello, :earth_africa:");
     }
 }
